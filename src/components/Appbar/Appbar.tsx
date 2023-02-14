41d41b07ecfeeceafa0bdd56ce548201b73c6118
Nageshwara sairam <113401242+nagesh161007@users.noreply.github.com>
2023-02-13 19:53:42 -0500

    fix title

--- src/components/Appbar/Appbar.tsx
+++ src/components/Appbar/Appbar.tsx
@@ -1,14 +1,25 @@
 import Image from "next/image";
 import React from "react";
-import View from "./icons/View";
 import styles from "./Appbar.module.scss";
 import AppLogo from "./icons/AppLogo";
 import CompanySwitcher from "../CompanySwitcher";
+import { useRouter } from "next/router";
+
+const titleConfig: Record<string, string> = {
+  "/projects": "PROJECTS",
+  "/": "HOME",
+};
 
 function Appbar() {
+  const router = useRouter();
+
+  const getByTitle = (pathname: string): string => {
+    return titleConfig[pathname];
+  };
+
   return (
     <div className={styles.appbar}>
-      <div className={styles.title}>HOME</div>
+      <div className={styles.title}>{getByTitle(router.pathname)}</div>
       <div className={styles.leftSection}>
         <div>
           <a href="https://drive.google.com/u/0/uc?id=1QZxevdNOQyc7111GlJ9Iqvllr2R04i_v&export=download">
