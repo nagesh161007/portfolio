747cb20b0e0eff6e85e8f4fa5c063e38dd4195ef
Nageshwara sairam <113401242+nagesh161007@users.noreply.github.com>
2023-04-07 23:42:40 -0400

    update projects

--- src/components/ProjectsPage/ProjectsPage.tsx
+++ src/components/ProjectsPage/ProjectsPage.tsx
@@ -5,11 +5,18 @@ import { ProjectCardModel } from "../ProjectCard/ProjectCard";
 
 const projects: ProjectCardModel[] = [
   {
-    title: "Portfolio Design",
-    image: "/skills/figma.svg",
-    link: "https://www.figma.com/file/ggw1lWZEWnWi93GnmjE7yO/My-portfolio?node-id=117%3A2&t=FKwpjr4TUA5o4n8A-1",
-    date: "Dec 2023",
-    description: "FIFA FUT Themed Personel portfolio design in Figma",
+    title: "Nike Customize Shoe",
+    image: "/skills/threejs.png",
+    link: "https://nike-customize-clone.vercel.app/",
+    date: "IN PROGRESS",
+    description: "Nike Customizable Shoe",
+  },
+  {
+    title: "Frontend Deployment Handbook",
+    image: "/skills/docusaurus.svg",
+    link: "https://frontend-deployment-handbook.vercel.app/",
+    date: "IN PROGRESS",
+    description: "Frontend Deployment Guide Handbook using Docusaurus",
   },
   {
     title: "PortFolio Website",
@@ -17,6 +24,13 @@ const projects: ProjectCardModel[] = [
     link: "https://nageshsairam.com",
     date: "Jan 2023",
     description: "FIFA FUT Themed Personel portfolio in Next.js",
+  },
+  {
+    title: "Portfolio Design",
+    image: "/skills/figma.svg",
+    link: "https://www.figma.com/file/ggw1lWZEWnWi93GnmjE7yO/My-portfolio?node-id=117%3A2&t=FKwpjr4TUA5o4n8A-1",
+    date: "Dec 2023",
+    description: "FIFA FUT Themed Personel portfolio design in Figma",
   },
 ];
 
